675740098d72aed1a36d15fad9a6704bc3894187
Nina Leung <nleung320@gmail.com>
2015-11-25 19:30:36 -0600

    added subtotal, tax, and total to the database

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
   def create
-    @new_order = Order.create(
+    product_purchased = Product.find_by(id: params[:product_id])
+    subtotal_calc = (product_purchased.price * params[:quantity].to_i)
+    tax_calc = (subtotal_calc * 0.1025).round(2)
+
+    new_order = Order.create(
       user_id: current_user.id,
-      quantity: params[:quantity],
-      product_id: params[:product_id]
+      quantity: params[:quantity].to_i,
+      product_id: params[:product_id],
+      subtotal: subtotal_calc,
+      tax: tax_calc,
+      total: subtotal_calc + tax_calc
       )
     flash[:success] = "Success! You've purchased this product!"
-    redirect_to "/orders/#{@new_order.id}"
+    redirect_to "/orders/#{new_order.id}"
   end
 
   def show
